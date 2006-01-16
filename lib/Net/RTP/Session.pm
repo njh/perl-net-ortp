c3fe959b92b83ea654a00af83c8a3577c5fe2810
Nicholas J Humfrey <njh@aelius.com>
2006-01-16 14:00:20 +0000

    Startings of additional classes

---
+++ lib/Net/RTP/Session.pm
@@ -0,0 +1,51 @@
+package Net::RTP::Session;
+
+################
+#
+# Net::RTP::Session
+#
+# Nicholas Humfrey
+# njh@ecs.soton.ac.uk
+#
+
+use strict;
+use Carp;
+
+
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Net::RTP::Session
+
+
+=head1 AUTHOR
+
+Nicholas Humfrey, njh@ecs.soton.ac.uk
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2005 University of Southampton
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+=cut
